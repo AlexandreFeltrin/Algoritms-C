0d5879d3202970ed0b21966bb05cd1b477f52cf0
Alexandre Feltrin <alexandrefeltrincasteller@gmail.com>
2023-10-23 11:25:26 -0300

    queue

--- Linkedlists/stack/queue.c
+++ Linkedlists/stack/queue.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct No{
     int n;
     struct no *prox;
 }No;
 
+
+
+typedef struct Fila{
+    No* base;
+    No* topo; 
+}Fila;
+
+Fila* criaFila(){
+    Fila* fila=(Fila*)malloc(sizeof(Fila));
+    fila->base=NULL;
+    fila->topo=NULL;
+    return fila;
+}
+
+int vazia(Fila* fila){
+    if(fila->base==NULL){
+        return 1;
+    }else{
+        return 0;
+    };
+}
+
+No* NovoNo(){
+    No *novo;
+    novo=(No*)malloc(sizeof(No));
+    printf("Digite o número que você deseja inserir"\n);
+    scanf("%d",&novo->n);
+    novo->prox=NULL;
+}
+
+void insereFila(Fila*f, No* no){
+    if (vazia(f))
+    {
+        f->base=no;
+        f->topo=no;
+    }else{
+        f->topo->prox=no;
+        f->topo=no;
+    }
+    
+}
+
+int menu(){
+    int opc;
+    printf("1.Listar o números de aviões aguardando na fila de decolagem\n");
+    printf("2.Autorizar a decolagem do primeiro avião da fila\n");
+    printf("3.Adicionar um avião a lista de espera\n");
+    printf("4.Listar todos os aviões na fila de espera\n ");
+    printf("5.Listar as características do primeiro avião da fila");
+    scanf("%d",&opc);
+    return(opc);
+}
--- teste/teste.c
+++ teste/teste.c
@@ -1,35 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        // Troca o elemento atual (arr[i]) pelo mínimo encontrado (arr[minIndex])
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+// Definindo a estrutura de um nó da fila
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Definindo a estrutura da fila
+typedef struct Queue {
+    Node* front; // Início da fila
+    Node* rear;  // Fim da fila
+} Queue;
+
+// Função para criar uma nova fila vazia
+Queue* createQueue() {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
+}
+
+// Função para verificar se a fila está vazia
+int isEmpty(Queue* queue) {
+    return (queue->front == NULL);
+}
+
+// Função para enfileirar um elemento na fila
+void enqueue(Queue* queue, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (isEmpty(queue)) {
+        queue->front = newNode;
+        queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
     }
 }
 
+// Função para desenfileirar um elemento da fila
+int dequeue(Queue* queue) {
+    if (isEmpty(queue)) {
+        printf("A fila está vazia.\n");
+        return -1; // Valor inválido para indicar erro
+    }
+    Node* frontNode = queue->front;
+    int data = frontNode->data;
+    queue->front = frontNode->next;
+    free(frontNode);
+
+    if (queue->front == NULL) {
+        queue->rear = NULL;
+    }
+
+    return data;
+}
+
+// Função para obter o elemento na frente da fila sem desenfileirar
+int peek(Queue* queue) {
+    if (isEmpty(queue)) {
+        printf("A fila está vazia.\n");
+        return -1; // Valor inválido para indicar erro
+    }
+    return queue->front->data;
+}
+
+// Função para imprimir os elementos da fila
+void printQueue(Queue* queue) {
+    Node* current = queue->front;
+    printf("Fila: ");
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    Queue* queue = createQueue();
 
-    printf("Array original: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    enqueue(queue, 1);
+    enqueue(queue, 2);
+    enqueue(queue, 3);
 
-    selectionSort(arr, n);
+    printQueue(queue);
 
-    printf("\nArray ordenado em ordem crescente: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    printf("Elemento na frente da fila: %d\n", peek(queue));
+
+    printf("Desenfileirando: %d\n", dequeue(queue));
+    printf("Desenfileirando: %d\n", dequeue(queue));
+
+    printQueue(queue);
 
     return 0;
 }
